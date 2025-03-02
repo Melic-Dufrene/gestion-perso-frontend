de66ffef2539015ecbe8921a1ce975e4aa4540eb
Mélic Dufrêne <melic.duf@gmail.com>
2025-03-02 19:58:56 +0100

    Update call to backend

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.1.137:3000/api", // Backend URL
+  baseURL: "http://localhost:3000/api", // Backend URL
   headers: { "Content-Type": "application/json" }
 });
 
