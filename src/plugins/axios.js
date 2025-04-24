aa66403c327c9ba5a379c99d34fabda758e98b4c
Mélic Dufrêne <melic.duf@gmail.com>
2025-04-24 18:04:22 +0200

    Correct hostname for backend

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api", // Backend URL
+  baseURL: "http://mondonjon.ddns.net:3000/api", // Backend URL
   headers: { "Content-Type": "application/json" }
 });
 
